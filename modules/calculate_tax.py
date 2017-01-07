16d763b9238bbf593c3da99b81d631c09327d144
sithu94 <saun2162@uni.sydney.edu.au>
2017-01-08 01:03:09 +1100

    DIrectory issues solved

--- modules/calculate_tax.py
+++ modules/calculate_tax.py
@@ -14,13 +14,13 @@ class Tax:
     def income_tax(income):
         if income <= 18200:
             active_tax = 0
-        elif (income>18200) && (income<=37000):
-            active_tax = 0.19*(active-18200)
-        elif (income>37000) && (income<=80000):
-            active_tax = 3572+(0.325*(active-37000))
-        elif (income>80000) && (income<=180000):
-            active_tax = 17547+(0.37*(active-80000))
+        elif (income>18200) & (income<=37000):
+            active_tax = 0.19*(income-18200)
+        elif (income>37000) & (income<=80000):
+            active_tax = 3572+(0.325*(income-37000))
+        elif (income>80000) & (income<=180000):
+            active_tax = 17547+(0.37*(income-80000))
         elif (income>180000):
-            active_tax = 54547+(0.45*(active-180000))
+            active_tax = 54547+(0.45*(income-180000))
 
         return active_tax
